6f5ec17f4d453b7cd9d078c32dc2a928a9195608
t <123@456>
2022-12-06 19:48:47 +0800

    modified:   osg/anti_alias/fxaa/testNode.cpp 	deleted:    osg/anti_alias/glsl/fxaa.glsl

--- osg/anti_alias/fxaa/testNode.cpp
+++ osg/anti_alias/fxaa/testNode.cpp
@@ -17,7 +17,7 @@ out vec4 clr;
 
 void main()
 {
-	gl_Position = gl_Vertex;
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 	gl_TexCoord[0] = gl_MultiTexCoord0;
 	clr = gl_Color;
 }
@@ -43,16 +43,114 @@ constexpr char fxaaShader[] = R"(
 
 #version 330 compatibility
 
+uniform vec2 tex_size;
 uniform sampler2D clr_tex;
 uniform sampler2D dep_tex;
 
-{0}
+const int search_step = 10;
+const int guess = 8;
+
+float luminace(vec3 clr)
+{
+	return 0.213 * clr.r + 0.715 * clr.g + 0.072 * clr.b;
+}
 
 void main()
-{{
+{
 	vec2 uv = gl_TexCoord[0].xy;
-	gl_FragColor = texture(clr_tex, uv);
-}}
+	vec2 des = vec2(1 / tex_size.x, 1 / tex_size.y);
+	float n = texture(clr_tex, uv + vec2(0,  des.y)).w; 
+	float s = texture(clr_tex, uv + vec2(0, -des.y)).w; 
+	float w = texture(clr_tex, uv + vec2(-des.x, 0)).w; 
+	float e = texture(clr_tex, uv + vec2( des.x, 0)).w; 
+	float c = texture(clr_tex, uv).w; 
+
+	float max_lu = max(c, max(max(n, s), max(w, e)));
+	float min_lu = min(c, min(min(n, s), min(w, e)));
+	float constract = max_lu - min_lu;
+	if(constract < 0.3){
+		gl_FragColor = texture(clr_tex, uv);
+		return;
+	}
+
+	float nw = texture(clr_tex, uv + vec2(-des.x,  des.y)).w; 
+	float sw = texture(clr_tex, uv + vec2(-des.x, -des.y)).w; 
+	float se = texture(clr_tex, uv + vec2( des.x, -des.y)).w; 
+	float ne = texture(clr_tex, uv + vec2( des.x,  des.y)).w; 
+	
+	float filter = (2 * (n + s + w + e) + nw + sw + se + ne) / 12;
+	filter = clamp(abs(filter - c) / constract, 0, 1);
+	float pix_blend = smoothstep(0, 1, filter);
+	pix_blend = pix_blend * pix_blend;
+
+	float vertical = abs(n + s - 2 * c) * 2 + abs(ne + se - 2 * e) + abs(nw + sw - 2 * w);
+	float horizontal = abs(e + w - 2 * c) * 2 + abs(ne + nw - 2 * n) + abs(se + sw - 2 * s);
+
+	bool is_hor = vertical > horizontal;
+	vec2 pix_step = vec2(des.x, 0);
+	if(is_hor)
+		pix_step = vec2(0, des.y);	
+	float positive = abs((is_hor ? n : e) - c);
+	float negative = abs((is_hor ? s : w) - c);
+
+	float gradient, opposite;
+	if(positive > negative){
+		gradient = positive;
+		opposite = is_hor ? n : e;
+	}
+	else{
+		pix_step = -pix_step;
+		gradient = negative;
+		opposite = is_hor ? s : w;
+	}
+	
+	vec2 uvedge = uv;
+	uvedge += pix_step * 0.5;
+	vec2 edge_step = is_hor ? vec2(des.x, 0) : vec2(0, des.y);
+	
+	float edge_lu = (c + opposite) * 0.5;
+	float gradient_threshold = gradient * 0.25;
+	float p_lu_delta, n_lu_delta, p_dis, n_dis;
+	int i = 0;
+	for(i = 1; i < search_step; i++){
+		p_lu_delta = texture(clr_tex, uvedge + i * edge_step).w - edge_lu;
+		if(abs(p_lu_delta) > gradient_threshold){
+			p_dis = i * (is_hor ? edge_step.x : edge_step.y);
+			break;
+		}
+	}
+	if(i == search_step)
+		p_dis = (is_hor ? edge_step.x : edge_step.y) * guess;
+	for(i = 1; i < search_step; i++){
+		n_lu_delta = texture(clr_tex, uvedge - i * edge_step).w - edge_lu;
+		if(abs(n_lu_delta) > gradient_threshold){
+			n_dis = i * (is_hor ? edge_step.x : edge_step.y);
+			break;
+		}
+	}
+	if(i == search_step)
+		n_dis = (is_hor ? edge_step.x : edge_step.y) * guess;
+
+	float edge_blend;
+	if(p_dis < n_dis){
+		if(sign(p_lu_delta) == sign(c - edge_lu))
+			edge_blend = 0;
+		else
+			edge_blend = 0.5 - p_dis / (p_dis + n_dis);
+	}
+	else{
+		if(sign(n_lu_delta) == sign(c - edge_lu))
+			edge_blend = 0;
+		else
+			edge_blend = 0.5 - n_dis / (p_dis + n_dis);
+	}
+	float final_blend = max(pix_blend, edge_blend);	
+
+	//vec3 clr1 = texture(clr_tex, uv).rgb;
+	//vec3 clr2 = texture(clr_tex, uv + pix_step).rgb;
+	//gl_FragColor = vec4(mix(clr1, clr2, final_blend), 1); 
+	gl_FragColor = texture(clr_tex, uv + pix_step * final_blend);
+}
 
 )";
 
@@ -78,13 +176,14 @@ TestNode::TestNode()
 
 void TestNode::init()
 {
-	_quad = createTexturedQuadGeometry(Vec3(0, -0.5, 0), Vec3(0.5, 0.5, 0), Vec3(-0.5, 0.5, 0));
+	//_quad = createTexturedQuadGeometry(Vec3(0, -0.5, 0), Vec3(0.5, 0.5, 0), Vec3(-0.5, 0.5, 0));
+	_quad = createTexturedQuadGeometry(Vec3(-0.5, -0.5, 0), Vec3(1, 0, 0), Vec3(0, 1, 0));
 	auto clr = new Vec3Array;
 	clr->push_back(Vec3(1, 0, 0));
-	clr->push_back(Vec3(0, 1, 0));
-	clr->push_back(Vec3(0, 0, 1));
-	clr->push_back(Vec3(1, 1, 0));
-	_quad->setColorArray(clr, Array::BIND_PER_VERTEX);
+	//clr->push_back(Vec3(0, 1, 0));
+	//clr->push_back(Vec3(0, 0, 1));
+	//clr->push_back(Vec3(1, 1, 0));
+    _quad->setColorArray(clr, Array::BIND_OVERALL);
 	auto ss = _quad->getOrCreateStateSet();
 	auto prg = new osg::Program;
 	prg->addShader(new osg::Shader(Shader::VERTEX, vertShader));
@@ -98,8 +197,8 @@ void TestNode::init()
 	_cam->setRenderOrder(Camera::PRE_RENDER);
 	_clrTex = new osg::Texture2D;
 	_clrTex->setInternalFormat(GL_RGBA);
-	_clrTex->setFilter(Texture::MIN_FILTER, Texture::NEAREST);
-	_clrTex->setFilter(Texture::MAG_FILTER, Texture::NEAREST);
+    _clrTex->setFilter(Texture::MIN_FILTER, Texture::LINEAR);
+    _clrTex->setFilter(Texture::MAG_FILTER, Texture::LINEAR);
 	_depTex = new osg::Texture2D;
 	_depTex->setInternalFormat(GL_DEPTH_COMPONENT);
 	_depTex->setFilter(Texture::MIN_FILTER, Texture::NEAREST);
@@ -118,12 +217,7 @@ void TestNode::init()
 	{
 		auto prg = new osg::Program;
 		prg->addShader(new osg::Shader(Shader::VERTEX, vertShader));
-
-		auto file_path = std::filesystem::path(__FILE__);
-		auto dir = file_path.parent_path();
-		auto fxstr = readFile(dir.string() + "../glsl/fxaa.glsl");
-		auto shader = std::format(fxaaShader, fxstr);
-		prg->addShader(new osg::Shader(Shader::FRAGMENT, shader));
+		prg->addShader(new osg::Shader(Shader::FRAGMENT, fxaaShader));
 		ss->setAttribute(prg);
 	}
 	addChild(_ssquad);
@@ -145,6 +239,9 @@ void TestNode::traverse(NodeVisitor& nv)
 			_depTex->setTextureSize(vp->width(), vp->height());
 			_depTex->dirtyTextureObject();
 			_cam->dirtyAttachmentMap();
+
+			auto ss = _ssquad->getOrCreateStateSet();
+            ss->getOrCreateUniform("tex_size", osg::Uniform::FLOAT_VEC2)->set(osg::Vec2(vp->width(), vp->height()));
 		}
 		_cam->accept(*cv);
 	}
