a724daa083deb343b27aba7224600400cc4c54c9
t <123@456>
2022-12-30 11:01:50 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   fxaa/testNode.cpp
    	modified:   msaa/testNode.cpp
    	modified:   msaa/testNode.h
    	modified:   taa/CMakeLists.txt
    	new file:   taa/glsl/out_frag.glsl
    	new file:   taa/glsl/taa_frag.glsl
    	new file:   taa/glsl/vel_frag.glsl
    	new file:   taa/glsl/vel_vert.glsl
    	modified:   taa/testNode.cpp

--- osg/anti_alias/fxaa/testNode.cpp
+++ osg/anti_alias/fxaa/testNode.cpp
@@ -8,7 +8,6 @@
 #include <osg/Geometry>
 #include <osgUtil/CullVisitor>
 
-
 const std::string vertShader = R"(
 
 #version 330 compatibility
@@ -53,8 +52,6 @@ void main()
 }
 
 )";
-
-
 
 constexpr char fxaaShader[] = R"(
 
@@ -175,96 +172,95 @@ void main()
 
 )";
 
-std::string readFile(const std::string &file)
+std::string readFile(const std::string& file)
 {
-	std::string ret;
-	auto f = fopen(file.c_str(), "rt");
-	if (!f) return ret;
-	fseek(f, 0, SEEK_END);
-	uint64_t len = ftell(f);
-	fseek(f, 0, SEEK_SET);
-	ret.resize(len);
-	fread(&ret[0], 1, len, f);
-	fclose(f);
-	return ret;
+  std::string ret;
+  auto f = fopen(file.c_str(), "rt");
+  if (!f)
+    return ret;
+  fseek(f, 0, SEEK_END);
+  uint64_t len = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  ret.resize(len);
+  fread(&ret[0], 1, len, f);
+  fclose(f);
+  return ret;
 }
 
 TestNode::TestNode()
 {
-	setCullingActive(false);
-	init();
+  setCullingActive(false);
+  init();
 }
 
 void TestNode::init()
 {
-	//_quad = createTexturedQuadGeometry(Vec3(0, -0.5, 0), Vec3(0.5, 0.5, 0), Vec3(-0.5, 0.5, 0));
-	_quad = createTexturedQuadGeometry(Vec3(-0.5, -0.5, 0), Vec3(1, 0, 0), Vec3(0, 1, 0));
-	auto clr = new Vec3Array;
-	clr->push_back(Vec3(1, 0, 0));
-	//clr->push_back(Vec3(0, 1, 0));
-	//clr->push_back(Vec3(0, 0, 1));
-	//clr->push_back(Vec3(1, 1, 0));
-    _quad->setColorArray(clr, Array::BIND_OVERALL);
-	auto ss = _quad->getOrCreateStateSet();
-	auto prg = new osg::Program;
-	prg->addShader(new osg::Shader(Shader::VERTEX, vertShader));
-	prg->addShader(new osg::Shader(Shader::FRAGMENT, fragShader));
-	ss->setAttribute(prg);
-	_quad->setCullingActive(false);
+  //_quad = createTexturedQuadGeometry(Vec3(0, -0.5, 0), Vec3(0.5, 0.5, 0), Vec3(-0.5, 0.5, 0));
+  _quad = createTexturedQuadGeometry(Vec3(-0.5, -0.5, 0), Vec3(1, 0, 0), Vec3(0, 1, 0));
+  auto clr = new Vec3Array;
+  clr->push_back(Vec3(1, 0, 0));
+  // clr->push_back(Vec3(0, 1, 0));
+  // clr->push_back(Vec3(0, 0, 1));
+  // clr->push_back(Vec3(1, 1, 0));
+  _quad->setColorArray(clr, Array::BIND_OVERALL);
+  auto ss = _quad->getOrCreateStateSet();
+  auto prg = new osg::Program;
+  prg->addShader(new osg::Shader(Shader::VERTEX, vertShader));
+  prg->addShader(new osg::Shader(Shader::FRAGMENT, fragShader));
+  ss->setAttribute(prg);
+  _quad->setCullingActive(false);
 
-	_cam = new Camera;
-	_cam->addChild(_quad);
-	_cam->setRenderOrder(Camera::PRE_RENDER);
-    _cam->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    _cam->setClearColor(osg::Vec4(0, 0, 0, 0));
-	_clrTex = new osg::Texture2D;
-	_clrTex->setInternalFormat(GL_RGBA);
-    _clrTex->setFilter(Texture::MIN_FILTER, Texture::LINEAR);
-    _clrTex->setFilter(Texture::MAG_FILTER, Texture::LINEAR);
-	_depTex = new osg::Texture2D;
-	_depTex->setInternalFormat(GL_DEPTH_COMPONENT);
-	_depTex->setFilter(Texture::MIN_FILTER, Texture::NEAREST);
-	_depTex->setFilter(Texture::MAG_FILTER, Texture::NEAREST);
-	_cam->attach(Camera::COLOR_BUFFER, _clrTex);
-	_cam->attach(Camera::DEPTH_BUFFER, _depTex);
-	_cam->setRenderTargetImplementation(_cam->FRAME_BUFFER_OBJECT);
+  _cam = new Camera;
+  _cam->addChild(_quad);
+  _cam->setRenderOrder(Camera::PRE_RENDER);
+  _cam->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  _cam->setClearColor(osg::Vec4(0, 0, 0, 0));
+  _clrTex = new osg::Texture2D;
+  _clrTex->setInternalFormat(GL_RGBA);
+  _clrTex->setFilter(Texture::MIN_FILTER, Texture::LINEAR);
+  _clrTex->setFilter(Texture::MAG_FILTER, Texture::LINEAR);
+  _depTex = new osg::Texture2D;
+  _depTex->setInternalFormat(GL_DEPTH_COMPONENT);
+  _depTex->setFilter(Texture::MIN_FILTER, Texture::NEAREST);
+  _depTex->setFilter(Texture::MAG_FILTER, Texture::NEAREST);
+  _cam->attach(Camera::COLOR_BUFFER, _clrTex);
+  _cam->attach(Camera::DEPTH_BUFFER, _depTex);
+  _cam->setRenderTargetImplementation(_cam->FRAME_BUFFER_OBJECT);
 
-	_ssquad = createTexturedQuadGeometry(Vec3(-1, -1, 0), Vec3(2, 0, 0), Vec3(0, 2, 0));
-	ss = _ssquad->getOrCreateStateSet();
-	ss->setTextureAttributeAndModes(0, _clrTex);
-	ss->setTextureAttributeAndModes(1, _depTex);
-	ss->getOrCreateUniform("clr_tex", Uniform::SAMPLER_2D)->set(0);
-	ss->getOrCreateUniform("dep_tex", Uniform::SAMPLER_2D)->set(1);
-	_ssquad->setCullingActive(false);
-	{
-		auto prg = new osg::Program;
-		prg->addShader(new osg::Shader(Shader::VERTEX, svShader));
-		prg->addShader(new osg::Shader(Shader::FRAGMENT, fxaaShader));
-		ss->setAttribute(prg);
-	}
-	addChild(_ssquad);
+  _ssquad = createTexturedQuadGeometry(Vec3(-1, -1, 0), Vec3(2, 0, 0), Vec3(0, 2, 0));
+  ss = _ssquad->getOrCreateStateSet();
+  ss->setTextureAttributeAndModes(0, _clrTex);
+  ss->setTextureAttributeAndModes(1, _depTex);
+  ss->getOrCreateUniform("clr_tex", Uniform::SAMPLER_2D)->set(0);
+  ss->getOrCreateUniform("dep_tex", Uniform::SAMPLER_2D)->set(1);
+  _ssquad->setCullingActive(false);
+  {
+    auto prg = new osg::Program;
+    prg->addShader(new osg::Shader(Shader::VERTEX, svShader));
+    prg->addShader(new osg::Shader(Shader::FRAGMENT, fxaaShader));
+    ss->setAttribute(prg);
+  }
+  addChild(_ssquad);
 }
 
 void TestNode::traverse(NodeVisitor& nv)
 {
-	osg::Group::traverse(nv);
+  osg::Group::traverse(nv);
 
-	auto cv = nv.asCullVisitor();
+  auto cv = nv.asCullVisitor();
 
-	if (cv)
-	{
-		auto vp = cv->getViewport();
-		if (_clrTex->getTextureWidth() != vp->width() || _clrTex->getTextureHeight() != vp->height())
-		{
-			_clrTex->setTextureSize(vp->width(), vp->height());
-			_clrTex->dirtyTextureObject();
-			_depTex->setTextureSize(vp->width(), vp->height());
-			_depTex->dirtyTextureObject();
-			_cam->dirtyAttachmentMap();
+  if (cv) {
+    auto vp = cv->getViewport();
+    if (_clrTex->getTextureWidth() != vp->width() || _clrTex->getTextureHeight() != vp->height()) {
+      _clrTex->setTextureSize(vp->width(), vp->height());
+      _clrTex->dirtyTextureObject();
+      _depTex->setTextureSize(vp->width(), vp->height());
+      _depTex->dirtyTextureObject();
+      _cam->dirtyAttachmentMap();
 
-			auto ss = _ssquad->getOrCreateStateSet();
-            ss->getOrCreateUniform("tex_size", osg::Uniform::FLOAT_VEC2)->set(osg::Vec2(vp->width(), vp->height()));
-		}
-		_cam->accept(*cv);
-	}
+      auto ss = _ssquad->getOrCreateStateSet();
+      ss->getOrCreateUniform("tex_size", osg::Uniform::FLOAT_VEC2)->set(osg::Vec2(vp->width(), vp->height()));
+    }
+    _cam->accept(*cv);
+  }
 }
